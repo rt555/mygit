43a4f4f5d56cdab4ce2176f4a46232776d50f26e
aa <2679109586@qq.com>
2019-03-25 20:09:51 +0800

    hallo

---
+++ .idea/inspectionProfiles/java/World.java
@@ -0,0 +1,16 @@
+/**
+ * Description: xxx<br/>
+ * Copyright(c), 2019, Jansonxu <br/>
+ * This program is protected by copyright laws.<br/>
+ * Date: 2019年03月25日
+ *
+ * @author 任涛
+ * @version : 1.0
+ */
+public class World {
+
+    public static void main(String []args) {
+        System.out.println("Hello World"); // 打印 Hello World
+    }
+
+}
